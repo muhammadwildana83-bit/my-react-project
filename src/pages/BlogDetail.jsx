b10b2a42bb8323c282a27abed695bab45d4724a8
muhammadwildana83-bit <muhammadwildana83@gmail.com>
2026-09-15 20:00:31 +0700

    update

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useParams, Link } from "react-router-dom";
-import "./Blogdetail.css";
+import "./BlogDetail.css";
 
 const formatDate = (dateString) => {
   if (!dateString) return "";
